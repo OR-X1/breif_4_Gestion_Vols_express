d60ce1cc41837aad274289a934ad25cfc126daae
Othmane_Rhazlani <oghazlani0@gmail.com>
2021-12-17 15:32:34 +0100

    done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const {connection} = require('./connexion');
 const { isBuffer } = require('util');
+
+var nodemailer = require('nodemailer');
 const app = express();
 
 const port = 3000
@@ -13,13 +16,20 @@ app.locals.data = {
 };
 
 
+// email -------------------------
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'contact.quizzy.com@gmail.com',
+    pass: 'quizzy.contact.2021'
+  }
+});
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: 'safiair'
-// });
+
+// end email -------------------------
+
+
+
 
 
 
@@ -49,12 +59,12 @@ app.get('/', (req, res) => {
             if(err) {
               throw err;
             }else{
-              
+
             }
-    
+
             res.render('index',{
                 title: 'Safi Air',
-                flights : row_flights,
+                flights : "",
                 villes : rows_ville
             });
           });
@@ -98,6 +108,7 @@ app.post('/', (req, res) => {
   let sql_avion = `SELECT * FROM avion where id_ville = ${id_ville} and date_depart = "${date_depart}" and h_depart like "${h_depart}" and capacity + ${nb_place} <= 20`;
   // and date_depart = ${date_depart} and h_depart like ${h_depart}
   let sql_ville = "SELECT * FROM ville";
+  let sql_extras = "SELECT * FROM extras";
     let query = connection.query(sql_avion, (err, rows_avion) => {
         if(err) {
           throw err;
@@ -111,12 +122,24 @@ app.post('/', (req, res) => {
             }else{
               
             }
-    
-            res.render('index',{
-                title: 'Safi Air',
-                flights : row_flights,
-                villes : rows_ville
-            });
+
+                  let row_villes = rows_ville;
+
+                  let query = connection.query(sql_extras, (err, rows_extras) => {
+                    if(err) {
+                      throw err;
+                    }else{
+                      
+                    }
+                    console.log(row_flights)
+            
+                    res.render('index',{
+                        title: 'Safi Air',
+                        flights : row_flights,
+                        villes : row_villes,
+                        extras : rows_extras
+                    });
+                  });
           });
         }
     });
@@ -130,33 +153,89 @@ app.post('/client_reserv', (req, res) => {
   let prenom = req.body.prenom;
   let email = req.body.email;
   let tele = req.body.tele;
+  let avion_id = req.body.avion_id;
+  let capacity = req.body.capacity;
 
+  // let sql_ticket = "SELECT reservation.*, avion.*, ville.* FROM reservation, avion, ville where  reservation.id_avion = avion.id and reservation.id_ville = ville.id";
+  let sql_ticket = "SELECT avion.*, ville.* FROM avion, ville where avion.id_ville = ville.id and avion.id = " + avion_id;
+
+    let query_ticket = connection.query(sql_ticket, (err, rows) => {
+        if(err) {
+          throw err;
+        }else{
+
+          var  test =  ejs.render(fs.readFileSync(__dirname+'/view/ticket.ejs','utf8'),{data:rows});
+          console.log(test);
+
+              var mailOptions = {
+                from: 'contact.quizzy.com@gmail.com',
+                to: 'oghazlani0@gmail.com',
+                subject: 'You got the ticket ',
+                // text: ` Fulle name : ${nom} ${prenom}
+                //         Vole id : ${avion_id}
+                // `,
+                html: test 
+              };
+
+              transporter.sendMail(mailOptions, (err, data) => {
+                if (err) {
+                    return log('Error occurs');
+                }
+              });
+        }     
+    });
 
   let sql_avion = `INSERT INTO client (nom, prenom, email, tele) VALUES ('${nom}', '${prenom}', '${email}', '${tele}');`;
-  // and date_depart = ${date_depart} and h_depart like ${h_depart}
-  // let sql_ville = "SELECT * FROM ville";
-    // let query = connection.query(sql_avion, (err, rows_avion) => {
-    //     if(err) {
-    //       throw err;
-    //     }else{
 
-          let row_flights = rows_avion;
+  let update_capacity_avion = `UPDATE avion SET capacity = ${capacity} + ${app.locals.data.nb_place}  WHERE avion.id = ${avion_id}`;
 
-          let query = connection.query(sql_ville, (err, rows_ville) => {
-            if(err) {
-              throw err;
-            }else{
-              
-            }
+    let query_avion = connection.query(sql_avion, (err, rows_avion) => {
+        if(err) {
+          throw err;
+        }else{
+        }
+      // res.redirect('/');
+
+    });
+
+    let last_client = "SELECT id FROM client ORDER BY ID DESC LIMIT 1";
+
+    // insert avion ----------------------------
+    let query_client = connection.query(last_client, (err, rows_last_client) => {
+      if(err) {
+        throw err;
+      }else{
+
+        let last_client_id = rows_last_client[0].id;
+        // console.log(rows_last_client[0].id);
+
+        let last_client = `INSERT INTO reservation (nb_place, id_client, id_avion, id_extras) VALUES (${app.locals.data.nb_place}, ${last_client_id}, ${avion_id}, 1)`;
+
+        // insert reservation ----------------------------
+        let query_client = connection.query(last_client, (err, rows_ville) => {
+          if(err) {
+            throw err;
+          }
+          // res.redirect('/');
+          // console.log(rows_ville);
+        });
+        // end insert reservation -----------------------------
+
+      }
+
+    });
+    // end insert avion -----------------------------
+
+    // insert avion ----------------------------
+    let query_update_capacity_avion = connection.query(update_capacity_avion, (err, rows_ville) => {
+      if(err) {
+        throw err;
+      }
+      res.redirect('/');
+    });
+    // end insert avion -----------------------------
+
     
-            res.render('index',{
-                title: 'Safi Air',
-                flights : row_flights,
-                villes : rows_ville
-            });
-          });
-    //     }
-    // });
 })
 
 
@@ -164,6 +243,7 @@ app.get('/', (req, res) => {
 
   let sql_avion = "SELECT * FROM avion";
   let sql_ville = "SELECT * FROM ville";
+
     let query = connection.query(sql_avion, (err, rows_avion) => {
         if(err) {
           throw err;
@@ -239,6 +319,24 @@ app.get('/extras', (req, res) => {
     });
 })
 
+app.post('/update_extras', (req, res) => {
+  let sql = `UPDATE extras SET choix = '${req.body.choix}', prix = '${req.body.prix}' WHERE extras.id = ${req.body.id}`;
+  let query = connection.query(sql, (err, results) => {
+      if(err) throw err;
+      res.redirect('/extras')
+  })
+});
+
+app.get('/delete_extras/:id', (req, res) => {
+  let sql = "DELETE FROM `extras` WHERE `extras`.`id` = " + req.params.id;
+  let query = connection.query(sql, (err, results) => {
+      if(err) throw err;
+      res.redirect('/extras')
+  })
+});
+
+//----------------------------------------------------------- ville -----------------------------------------------------------
+
 app.get('/ville', (req, res) => {
 
   let sql = "SELECT * FROM ville";
@@ -248,6 +346,39 @@ app.get('/ville', (req, res) => {
         res.render('ville',{
             title: 'CRUD /database',
             villes : rows
+        });
+    });
+})
+
+app.post('/update_ville', (req, res) => {
+  let sql = `UPDATE ville SET name_ville = '${req.body.name_ville}' WHERE ville.id = ${req.body.id}`;
+  let query = connection.query(sql, (err, results) => {
+      if(err) throw err;
+      res.redirect('/ville')
+  })
+});
+
+app.get('/delete_ville/:id', (req, res) => {
+  let sql = "DELETE FROM `ville` WHERE `ville`.`id` = " + req.params.id;
+  let query = connection.query(sql, (err, results) => {
+      if(err) throw err;
+      res.redirect('/ville')
+  })
+});
+
+//----------------------------------------------------------- end ville -----------------------------------------------------------
+
+
+app.get('/reservation', (req, res) => {
+
+  let sql = "SELECT reservation.*, client.*, avion.* FROM reservation, client, avion where reservation.id_client = client.id and reservation.id_avion = avion.id";
+
+    let query = connection.query(sql, (err, rows) => {
+        if(err) throw err;
+
+        res.render('reservation',{
+            title: 'reservation',
+            reservations : rows
         });
     });
 })
